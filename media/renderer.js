0bafee663aeee6c954dadde9b3512f24b7d01c95
António Dias <adias@mosano.eu>
2021-03-09 11:26:38 +0000

    Draw heat

--- media/renderer.js
+++ media/renderer.js
@@ -30,6 +30,7 @@ const render = (data, keyboard) => {
 
   const ctx = canvas.getContext('2d')
   console.log(keyboard)
+  console.log(data)
   // draw keys
   for (let key of keyboard.keys) {
     // border
@@ -40,23 +41,46 @@ const render = (data, keyboard) => {
       key.width * unit,
       key.height * unit
     )
+
     // key face
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'
+    ctx.fillStyle = `rgba(255, 255, 255, 0.8)`
     ctx.fillRect(
       key.x * unit + 2,
       key.y * unit + 2,
       key.width * unit - 4,
       key.height * unit - 4
     )
+
+    // draw heat
+    let weight = 0
+    for (let l of key.labels) {
+      if (l) {
+        const label = l.toLowerCase()
+        if (label && label in data) {
+          weight += data[label].weight
+        }
+      }
+    }
+
+    if (weight > 0) {
+      // convert to 0-1 then normalize it to have at least 0.1 opacity
+      const opacity = (weight / 100) * 0.75 + 0.25
+      ctx.fillStyle = `rgba(255, 0, 0, ${opacity})`
+      ctx.fillRect(
+        key.x * unit + 2,
+        key.y * unit + 2,
+        key.width * unit - 4,
+        key.height * unit - 4
+      )
+    }
   }
 }
 
 window.addEventListener('message', (ev) => {
   const { keypresses, keyboard } = ev.data
   if (keypresses) {
-    const data = weightData(keypresses, variant)
     if (keyboard) {
-      render(data, keyboard)
+      render(keypresses, keyboard)
     } else {
       renderLoadFail()
     }
