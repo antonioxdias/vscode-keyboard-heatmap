fb90e0aa145ba7e5294bdc9e259849a4b8ff6e6c
António Dias <adias@mosano.eu>
2021-03-09 10:07:15 +0000

    Add canvas. Rough draw keys.

--- media/renderer.js
+++ media/renderer.js
@@ -1,4 +1,5 @@
 const canvasContainerId = 'canvas-container'
+const unit = 54
 
 const renderLoadFail = () => {
   const titleEl = document.getElementById('title')
@@ -9,15 +10,50 @@ const renderLoadFail = () => {
 }
 
 const render = (data, keyboard) => {
+  const container = document.getElementById(canvasContainerId)
+  if (!container) {
+    return
+  }
+
   const titleEl = document.getElementById('title')
   if (titleEl) {
     titleEl.innerHTML = 'Hurray!'
+  }
+
+  // create canvas
+  const canvas = document.createElement('canvas')
+  canvas.id = 'keyboard-heatmap'
+  canvas.width = container.clientWidth
+  canvas.height = container.clientHeight
+  // append to container
+  container.appendChild(canvas)
+
+  const ctx = canvas.getContext('2d')
+  console.log(keyboard)
+  // draw keys
+  for (let key of keyboard.keys) {
+    // border
+    ctx.fillStyle = 'rgba(100, 100, 100, 0.8)'
+    ctx.fillRect(
+      key.x * unit,
+      key.y * unit,
+      key.width * unit,
+      key.height * unit
+    )
+    // key face
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)'
+    ctx.fillRect(
+      key.x * unit + 2,
+      key.y * unit + 2,
+      key.width * unit - 4,
+      key.height * unit - 4
+    )
   }
 }
 
 window.addEventListener('message', (ev) => {
   const { keypresses, keyboard } = ev.data
-  console.log(keypresses, keyboard)
+
   if (keypresses) {
     if (keyboard) {
       render(keypresses, keyboard)
